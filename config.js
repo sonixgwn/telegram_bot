9bd4b714c132c80bab1576eb3c481109b2d99bfd
Peter Leonard <peter.leonard.lie@gmail.com>
2025-03-04 22:25:51 +0700

    update: comment out old telegram token and add new one for pwcplayalpha_bot

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-    telegramToken: "7548403531:AAG1DHdQgZ2mQ1Rv6ci99oYQRFfS-CbYFWM", //@pwcdev_bot
+    // telegramToken: "7548403531:AAG1DHdQgZ2mQ1Rv6ci99oYQRFfS-CbYFWM", //@pwcdev_bot
     telegramToken: "7890415940:AAG97WUcW2sMXhHDzWgM_o63_jytzk3zXvQ", //@pwcplayalpha_bot
     apiBaseUrl: "http://localhost:5050",
     telegramApiUrl: "http://localhost:5050/telegram",
