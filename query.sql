f2d777e563588baccc4fd2627cca1cabee449f3c
Peter Leonard <peter.leonard.lie@gmail.com>
2025-04-20 19:57:56 +0700

    feat: add telegram and telegram_chat_id columns to users table

--- query.sql
+++ query.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `ingaming`.`users` 
+ALTER TABLE `users` 
 ADD COLUMN `telegram` TINYINT NOT NULL DEFAULT 0 AFTER `notes`,
 ADD COLUMN `telegram_chat_id` VARCHAR(45) NULL AFTER `telegram`;
